06dd84a8ec8dad081e8d14b13b4401a8a5224ea2
ailurus1991 <pandachow19910411@gmail.com>
2019-01-24 18:36:36 +0800

    u

--- demo.py
+++ demo.py
@@ -11,9 +11,8 @@ lm_res = lm_model([["ho", "thera"], ["thore"], ["oui"]])
 for _ in lm_res:
     print(_)
 
-bm_model = BrillMooreModel(dictionary_path="/home/guokr/DeepPav/deeppav/dictionary/new_static_dict.pkl",
+bm_model = BrillMooreModel(dictionary_path="./sample_checkpoint/new_static_dict.pkl",
                            model_path="./sample_checkpoint/error_model.tsv", candidates_count=1)
-
 
 origin_sent = "so do you kno where is the opple ?"
 bm_res = bm_model([origin_sent.split()])
